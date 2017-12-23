aaef677ff7f27436a16ce1c7700caaafab012226
quinton <justin.quinton@gmail.com>
2017-12-23 12:28:09 -0500

    Added directory scan -improved error handling -fixed main regex for currency conversion -fixed date regex for different statement format -fixed amount capture -added checksum FP tolerance -changed argument format

--- index.js
+++ index.js
@@ -2,66 +2,98 @@ const minimist = require('minimist');
 const pdftUtil = require('pdf-to-text');
 const fs = require('fs');
 const json2csv = require('json2csv');
+const os = require('os');
+const path = require('path');
 
 const parsedArgs = minimist(process.argv);
-const inFile = parsedArgs['file'];
+const inFile = parsedArgs['in-file'];
+const inDirectory = parsedArgs['in-directory'];
 const outFile = parsedArgs['out-file'];
 
 if (!outFile) {
     console.error('Output file must be specified');
 } else if (inFile) {
-    pdftUtil.pdfToText(inFile, { format: 'table' }, function (error, data) {
-        if (error) {
-            console.error(`Error reading file ${inFile}:`, error);
-        } else {
-            const yearMatches = /Statement Period\s+\w{3}\s+\d{1,2},\s+(\d{4})/mg.exec(data);
-            const year = yearMatches[1];
-            const records = [];
-            const regex = /(\d{3})\s+(\w{3}\s+\d{1,2})\s+\w{3}\s+\d{1,2}\s+(.+?)\s+([\d,]+?\.\d{2})(-?)/mg;
-            let match;
-            while (match = regex.exec(data)) {
-                const date = new Date(`${year}-${match[2].replace(' ', '-')}`);
-                records.push({
-                    recordId: parseInt(match[1]),
-                    date: `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`,
-                    item: match[3],
-                    amount: parseFloat(match[5] + match[4]),
-                });
-            }
-
-            for (let i = 1; i < records.length; ++i) {
-                if (records[i - 1].recordId + 1 !== records[i].recordId) {
-                    console.error(`Records ${i - 1} and ${i} are not contiguous`);
-                }
-            }
-
-            const paymentMatches = /Payments\/credits[\s-$]+([\d,.]+)/mg.exec(data);
-            const payments = parseFloat(paymentMatches[1].replace(',', ''));
-            const purchasesMatches = /Purchases\/charges[\s\+$]+([\d,.]+)/mg.exec(data);
-            const purchases = parseFloat(purchasesMatches[1].replace(',', ''));
-            let checksum = purchases - payments;
-
-            for (let record of records) {
-                checksum -= record.amount;
-            }
-
-            if (!checksum) {
-                console.error(`Checksum failure:`, checksum);
-            }
-
-            json2csv({
-                data: records,
-                fields: ['date', 'item', 'amount']
-            }, function (error, csvString) {
-                if (error) {
-                    console.error('Failed to convert records to csv:', error);
-                } else {
-                    fs.writeFileSync(outFile, csvString);
-                }
-            });
-        }
-    });
+    fileToRecords(inFile)
+        .then(records => recordsToCsvFile(records, outFile))
+        .catch(error => console.error(`Failed converting ${inFile} to records:  ${error}`));
+} else if (inDirectory) {
+    Promise.all(fs.readdirSync(inDirectory).map(file => fileToRecords(path.join(inDirectory, file))))
+        .then(recordNestedArray => [].concat.apply([], recordNestedArray))
+        .then(records => recordsToCsvFile(records, outFile))
+        .catch(error => console.error(`Failed converting ${inDirectory} to records:  ${error}`));
 } else {
-    console.info('File must be specified');
+    console.info('in-file or in-directory must be specified');
 }
 
+function fileToRecords(inFile) {
+    return new Promise((resolve, reject) => {
+        pdftUtil.pdfToText(inFile, { format: 'table' }, (error, data) => {
+            try {
+                if (error) {
+                    reject(error);
+                } else {
+                    const yearMatches = /Statement date +\w{3} +\d{1,2}, +(\d{4})/mg.exec(data);
+                    const year = yearMatches[1];
+                    const records = [];
+                    const regex = /(\d{3}) +(\w{3} +\d{1,2}) +(?:\w{3} +\d{1,2} +)?(.+?)(?:AMT +(?:[\d,]+?\.\d{2}-?)? (?:[\w ]*?)?)? +([\d,]+?\.\d{2})(-?)/g;
+                    let match;
+                    while (match = regex.exec(data)) {
+                        const date = new Date(`${year}-${match[2].replace(' ', '-')}`);
+                        records.push({
+                            recordId: parseInt(match[1]),
+                            amount: parseFloat(match[5] + match[4].replace(',', '')),
+                            item: match[3],
+                            date: `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`,
+                        });
+                    }
+
+                    const validationErrors = [];
+                    for (let i = 1; i < records.length; ++i) {
+                        if (records[i - 1].recordId + 1 !== records[i].recordId) {
+                            validationErrors.push(`Records ${i - 1} and ${i} are not contiguous`);
+                        }
+                    }
+
+                    const paymentMatches = /Payments\/credits[\s-$]+([\d,.]+)/mg.exec(data);
+                    const payments = parseFloat(paymentMatches[1].replace(',', ''));
+                    const purchasesMatches = /Purchases\/charges[\s\+$]+([\d,.]+)/mg.exec(data);
+                    const purchases = parseFloat(purchasesMatches[1].replace(',', ''));
+                    let checksum = purchases - payments;
+
+                    for (let record of records) {
+                        checksum -= record.amount;
+                    }
+
+                    if (Math.abs(checksum) > 0.01) {
+                        validationErrors.push(`Checksum failure:  ${checksum}`);
+                    }
+
+                    if (validationErrors.length > 0) {
+                        reject(validationErrors.join(os.EOL));
+                    } else {
+                        resolve(records);
+                    }
+
+                }
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }).catch(error => {
+        throw new Error(`Error reading file ${inFile}:  ${error}`);
+    });
+}
+
+function recordsToCsvFile(records, outFile) {
+    json2csv({
+        data: records,
+        fields: ['date', 'item', 'amount']
+    }, (error, csvString) => {
+        if (error) {
+            console.error('Failed to convert records to csv:', error);
+        } else {
+            fs.writeFileSync(outFile, csvString);
+            console.info(`Successfully wrote output to ${outFile}`);
+        }
+    });
+}
