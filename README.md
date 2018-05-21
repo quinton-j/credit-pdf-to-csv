d34340eb10b7d0a031d0feebec1a431007411d7b
quinton <justin.quinton@gmail.com>
2018-05-21 10:46:36 -0400

    Restructing for multiple input banks - reject unknown files with good message - dependency version updates

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # credit-pdf-to-csv
+
+## Description
+
+Parses, categorizes, and converts to transactions from a VISA pdf to a CSV file.  User must create a `category.json` file in the running directory to categorize the transactions.
+
+## Categories
+
+`category.json` file must be of the form
+
+```json
+{
+    "forced": [
+        {
+            "date": "2014-12-11",
+            "item": "specific description text",
+            "category": "car"
+        }
+    ],
+    "grocery": [
+        "GROCERY",
+        "Store 1 description",
+        "Store 2 description"
+    ]
+}
+```
+
+The forced section is expected and can be used to target an exact record to a new category.  All other sections are processed for an includes match until one is found.  If no matches are found, the record is marked as "unclassified".
+
+## Usage
+
+The script can run against an individual file:
+
+`node index.js --in-file myVisaFile.pdf --out-file myVisaRecords.csv`
+
+Or against a directory:
+
+``node index.js --in-directory ./myVisaFiles/ --out-file myVisaRecords.csv``
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const minimist = require('minimist');
 const pdftUtil = require('pdf-to-text');
 const fs = require('fs');
-const json2csv = require('json2csv');
+const json2csv = require('json2csv').parse;
 const os = require('os');
 const path = require('path');
 
@@ -38,52 +38,13 @@ function fileToRecords(inFile) {
                 if (error) {
                     reject(error);
                 } else {
-                    const yearMatches = /Statement date +(\w{3}) +\d{1,2}, +(\d{4})/mg.exec(data);
-                    const statementMonth = yearMatches[1];
-                    const statementYear = parseInt(yearMatches[2]);
-                    const records = [];
-                    const regex = /(\d{3}) +(\w{3} +\d{1,2}) +(?:\w{3} +\d{1,2} +)?(.+?)(?:AMT +(?:[\d,]+?\.\d{2}-?)? (?:[\w ]*?)?)? +([\d,]+?\.\d{2})(-?)(?:[^%])/g;
-                    let match;
-                    while (match = regex.exec(data)) {
-                        const date = new Date(`${statementYear}-${match[2].replace(' ', '-')}`);
-                        if (statementMonth === 'Jan' && date.getMonth() === 11) {
-                            date.setFullYear(statementYear - 1);
-                        }
-                        records.push({
-                            recordId: parseInt(match[1]),
-                            amount: parseFloat(match[5] + match[4].replace(',', '')),
-                            item: match[3],
-                            date: `${date.getFullYear()}-${ensureTwoDigits(date.getMonth() + 1)}-${ensureTwoDigits(date.getDate())}`,
-                        });
-                    }
-
-                    const validationErrors = [];
-                    for (let i = 1; i < records.length; ++i) {
-                        if (records[i - 1].recordId + 1 !== records[i].recordId) {
-                            validationErrors.push(`Records ${i - 1} and ${i} are not contiguous`);
-                        }
-                    }
-
-                    const paymentMatches = /Payments\/credits[\s-$]+([\d,.]+)/mg.exec(data);
-                    const payments = parseFloat(paymentMatches[1].replace(',', ''));
-                    const purchasesMatches = /Purchases\/charges[\s\+$]+([\d,.]+)/mg.exec(data);
-                    const purchases = parseFloat(purchasesMatches[1].replace(',', ''));
-                    let checksum = purchases - payments;
-
-                    for (let record of records) {
-                        checksum -= record.amount;
-                    }
-
-                    if (Math.abs(checksum) > 0.01) {
-                        validationErrors.push(`Checksum failure:  ${checksum}`);
-                    }
+                    const { records, validationErrors } = parseRecord(data);
 
                     if (validationErrors.length > 0) {
                         reject(validationErrors.join(os.EOL));
                     } else {
                         resolve(records);
                     }
-
                 }
             } catch (error) {
                 reject(error);
@@ -103,17 +64,13 @@ function ensureTwoDigits(number) {
 }
 
 function recordsToCsvFile(records, outFile) {
-    json2csv({
-        data: records,
-        fields: ['date', 'item', 'category', 'amount'],
-    }, (error, csvString) => {
-        if (error) {
-            console.error('Failed to convert records to csv:', error);
-        } else {
-            fs.writeFileSync(outFile, csvString);
-            console.info(`Successfully wrote output to ${outFile}`);
-        }
-    });
+    try {
+        const csvString = json2csv(records, { fields: ['date', 'item', 'category', 'amount'] });
+        fs.writeFileSync(outFile, csvString);
+        console.info(`Successfully wrote output to ${outFile}`);
+    } catch (error) {
+        console.error('Failed to convert records to csv:', error);
+    }
 }
 
 function categorizeRecords(records) {
@@ -137,4 +94,56 @@ function getCategory(record) {
     }
 
     return 'unclassified';
+}
+
+function parseRecord(data) {
+    if(/Scotia.*VISA.*[cC]ard/mg.test(data)) {
+        return parseScotiabankRecord(data);
+    } else {
+        throw new Error('Unrecognized credit file');
+    }
+}
+
+function parseScotiabankRecord(data) {
+    const yearMatches = /Statement date +(\w{3}) +\d{1,2}, +(\d{4})/mg.exec(data);
+    const statementMonth = yearMatches[1];
+    const statementYear = parseInt(yearMatches[2]);
+    const records = [];
+    const regex = /(\d{3}) +(\w{3} +\d{1,2}) +(?:\w{3} +\d{1,2} +)?(.+?)(?:AMT +(?:[\d,]+?\.\d{2}-?)? (?:[\w ]*?)?)? +([\d,]+?\.\d{2})(-?)(?:[^%])/g;
+    let match;
+    while (match = regex.exec(data)) {
+        const date = new Date(`${statementYear}-${match[2].replace(' ', '-')}`);
+        if (statementMonth === 'Jan' && date.getMonth() === 11) {
+            date.setFullYear(statementYear - 1);
+        }
+        records.push({
+            recordId: parseInt(match[1]),
+            amount: parseFloat(match[5] + match[4].replace(',', '')),
+            item: match[3],
+            date: `${date.getFullYear()}-${ensureTwoDigits(date.getMonth() + 1)}-${ensureTwoDigits(date.getDate())}`,
+        });
+    }
+
+    const validationErrors = [];
+    for (let i = 1; i < records.length; ++i) {
+        if (records[i - 1].recordId + 1 !== records[i].recordId) {
+            validationErrors.push(`Records ${i - 1} and ${i} are not contiguous`);
+        }
+    }
+
+    const paymentMatches = /Payments\/credits[\s-$]+([\d,.]+)/mg.exec(data);
+    const payments = parseFloat(paymentMatches[1].replace(',', ''));
+    const purchasesMatches = /Purchases\/charges[\s\+$]+([\d,.]+)/mg.exec(data);
+    const purchases = parseFloat(purchasesMatches[1].replace(',', ''));
+    let checksum = purchases - payments;
+
+    for (let record of records) {
+        checksum -= record.amount;
+    }
+
+    if (Math.abs(checksum) > 0.01) {
+        validationErrors.push(`Checksum failure:  ${checksum}`);
+    }
+
+    return { records, validationErrors };
 }
